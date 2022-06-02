ba61fd08a1fae0ca361c0042d4295d3428333fb3
PlewkaMaciej <82128422+PlewkaMaciej@users.noreply.github.com>
2022-06-02 21:16:48 +0200

    chance probability to percent

--- src/components/YourCountry.js
+++ src/components/YourCountry.js
@@ -3,7 +3,7 @@ import world from "../photos/world.png"
 function YourCountry({yourCountry}){
     return(<>
         {yourCountry.map((value,index)=>{
-            return  <> <div key={index} className="country-and-world-photo-container"> <p >Your country id: {value.country_id} probabilty: {value.probability}</p> <img className="world-photo" src={world}></img></div></>
+            return  <> <div key={index} className="country-and-world-photo-container"> <p >Your country id: {value.country_id} probabilty: {(value.probability*100).toFixed(2)}%</p> <img className="world-photo" src={world}></img></div></>
         })}
         </>
         
